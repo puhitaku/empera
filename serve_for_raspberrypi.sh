a82cc5188232b4a7902c1a976a055a99db33c01f
Takumi Sueda <puhitaku@gmail.com>
2020-05-03 02:18:10 +0900

    Update RasPi example

--- serve_for_raspberrypi.sh
+++ serve_for_raspberrypi.sh
@@ -2,6 +2,10 @@
 # This script proxies two repositories:
 #    Raspbian mirror (mainly GETs from ftp.jaist.ac.jp/raspbian/)
 #    Raspberry Pi Foundation (mainly GETs from archive.raspberrypi.org/debian/)
+#
+# Write sources.list like:
+#    deb http://localhost:8080/raspbian buster main
+#    deb http://localhost:8081/debian buster main
 
 go run empera.go \
     -rule "local=localhost:8080, remote=ftp.jaist.ac.jp" \
